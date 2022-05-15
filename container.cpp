1674f3b649cf228bce68a5cd4e944f3ea2acb9c2
Elias <ebalint896@gmail.com>
2022-05-15 16:00:34 +0200

    operators done

--- container.cpp
+++ container.cpp
@@ -12,14 +12,17 @@ Container::Container(int param)
 Container::Container(Container &other)
 {
     this->table_size = other.table_size;
-    // TODO: copy (std::vector miatt kérdéses)
+    table = new std::list<std::pair<KEY, VALUE>>[table_size];
+    for (int i = 0; i < table_size; i++)
+    {
+        table[i] = other.table[i];
+    }
 }
 
 Container::~Container()
 {
-    // TODO: finish
+    delete[] table;
 }
-
 
 void Container::insert(KEY &key, VALUE &value)
 {
@@ -36,15 +39,20 @@ bool Container::insert_or_assign(KEY &key, VALUE &value)
     // hash key
     int index = key.hash();
     index = index % table_size;
+    // TODO: ezt befejezni
     return true;
 }
 
 bool Container::erase(KEY &key)
 {
+    // hash key
+    int index = key.hash();
+    index = index % table_size;
+    // TODO: find in list:
     return true;
 }
 
-std::pair<KEY, VALUE> Container::find(KEY &key) //TODO: find() máshogy működik az std::map-ben
+std::pair<KEY, VALUE> Container::find(KEY &key) // TODO: find() máshogy működik az std::map-ben
 {
     int index = key.hash();
     index = index % table_size;
@@ -55,4 +63,32 @@ std::pair<KEY, VALUE> Container::find(KEY &key) //TODO: find() máshogy működi
             return j;
         }
     }
+    // TODO: ha nincs benne, akkor valami más, preferably iterator
+}
+
+void Container::operator=(Container &other)
+{
+    this->table_size = other.table_size;
+    //
+    for (int i = 0; i < table_size; i++)
+    {
+        table[i] = other.table[i];
+    }
+}
+
+bool Container::operator==(Container &other)
+{
+    if (this->table_size != other.table_size)
+        return false;
+    for (int i = 0; i < table_size; i++)
+    {
+        if (table[i] != other.table[i])
+            return false;
+    }
+    return true;
+}
+
+VALUE Container::operator[](KEY key)
+{
+    return find(key).second;
 }
--- container.h
+++ container.h
@@ -3,18 +3,20 @@
 
 #include "key.h"
 #include <list>
+#include <vector>
 #include <utility>
 #include <string>
 
-//template <class KEY, class VALUE>
+// template <class KEY, class VALUE>
 typedef StrKey KEY;
 typedef std::string VALUE;
 
-//egyelőre nemgenerikus megvalósítás, std::string kulcs és std::string értékkel
+// egyelőre nemgenerikus megvalósítás, StrKey kulcs és std::string értékkel
 class Container
 {
 private:
-    std::list<std::pair<KEY, VALUE>> *table; //TODO: std::vector?
+    std::list<std::pair<KEY, VALUE>> *table; 
+    //std::vector<std::list<std::pair<KEY, VALUE>>> table; std::vector
     int table_size;
 
 public:
@@ -27,25 +29,25 @@ public:
     // hozzáadni - ilyenkor kulcsot és értéket is kell adni paraméterként
     void insert(KEY &key, VALUE &value);
     // módosításra az std::map insert_or_assign metódusához hasonló metódus
-    //ilyenkor kulcsot és értéket is kell adni paraméterként
+    // ilyenkor kulcsot és értéket is kell adni paraméterként
     bool insert_or_assign(KEY &key, VALUE &value);
     // törölni - ilyenkor kulcsot kell adni paraméterként
     bool erase(KEY &key);
     // lekérdezni h a kulcshoz van-e érték tárolva
-    //ilyenkor a kulcsot kell adni paraméterként
+    // ilyenkor a kulcsot kell adni paraméterként
     std::pair<KEY, VALUE> find(KEY &key);
-    //esetleg:
-    //VALUE find(KEY key);
-    //vagy bool visszatérési érték
+    // esetleg:
+    // VALUE find(KEY key);
+    // vagy bool visszatérési érték
     // kiírja a tároló tartalmát (teszteléshez)
     void print();
-    //operátorok:~~~~~~~~~~~~~~~
+    // operátorok:~~~~~~~~~~~~~~~
     //értékadás operátor
-    bool operator=(Container &other);
-    //egyenlőség vizsgálat
+    void operator=(Container &other);
+    // egyenlőség vizsgálat
     bool operator==(Container &other);
-    //indexelő operátor
-    bool operator[](Container &other);
+    // indexelő operátor
+    VALUE operator[](KEY key);
 };
 
 #endif
