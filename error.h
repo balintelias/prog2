d44815147045d848ddf85b0f38c41bb00b8586f2
Elias <ebalint896@gmail.com>
2022-05-16 20:15:45 +0200

    lost everything

---
+++ error.h
@@ -0,0 +1,20 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+#include <exception>
+#include <string>
+
+class NotInList : public std::exception
+{
+private:
+    /* data */
+public:
+    NotInList(/* args */);
+    ~NotInList();
+    std::string what()
+    {
+        return "No instance of key in container.";
+    }
+};
+
+#endif
