dc0cd2adb17fb541e6403dd466b730aa30250bdf
Elias <ebalint896@gmail.com>
2022-05-17 16:54:59 +0200

    IntKey class created

--- key.h
+++ key.h
@@ -21,4 +21,19 @@ public:
     }
 };
 
+class IntKey
+{
+private:
+    int key;
+public:
+    IntKey(int param);
+    void setKey(int param);
+    int getKey();
+    int hash(); // implementation: std::hash
+    bool operator==(IntKey const &other) const
+    {
+        return key == other.key;
+    }
+};
+
 #endif
